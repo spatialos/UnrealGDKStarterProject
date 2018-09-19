1115fcbf90e85f86357df974c1504bf67d142c90
Daniel Hall <danielhall@improbable.io>
2018-09-19 18:24:39 +0100

    Updates

--- LaunchSpatial.bat
+++ LaunchSpatial.bat
@@ -3,9 +3,10 @@ echo Starting a local Spatial deployment with the default launch configuration. 
 
 pushd "%~dp0\spatial"
 
+spatial worker build build-config 
+
 rem To launch a local Spatial deployment with a different launch configuration, change the default_launch.json argument to the name of your configuration file, 
 rem for example one_worker_test.json or two_worker_test.json
-spatial worker build build-config default_launch.json
+spatial local launch default_launch.json
 
-spatial local launch
 popd
