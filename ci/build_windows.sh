d01b033fafa5d8b6e20e67609240652d086be5ca
Daniel Hall <danielhall@improbable.io>
2018-09-27 16:52:11 +0100

    New CI scripts (#20)
    * New CI scripts
    
    * Fixup

--- ci/build_windows.sh
+++ ci/build_windows.sh
@@ -10,19 +10,19 @@ markStartOfBlock "$0"
 
 source ci/prepare_build_environment.sh
 
-markStartOfBlock "Build the StarterProject (Windows)"
+markStartOfBlock "Build the Starter project (Windows)"
   # Build each target to ensure scripts are correct, skipping code generation on all but the first to save some time.
-  Game/Scripts/BuildWorker.bat "StarterProjectEditor" "Win64" "Development" "StarterProject.uproject"
+  Game/Plugins/UnrealGDK/SpatialGDK/Build/Scripts/BuildWorker.bat "StarterProjectEditor" "Win64" "Development" "StarterProject.uproject"
   if [[ ! -f "spatial/build/assembly/worker/UnrealEditor@Windows.zip" ]]; then
     echo "Editor was not properly built."
     exit 1
   fi
 
-  Game/Scripts/BuildWorker.bat "StarterProject" "Win64" "Development" "StarterProject.uproject" --skip-codegen
+  Game/Plugins/UnrealGDK/SpatialGDK/Build/Scripts/BuildWorker.bat "StarterProject" "Win64" "Development" "StarterProject.uproject" --skip-codegen
   if [[ ! -f "spatial/build/assembly/worker/UnrealClient@Windows.zip" ]]; then
      echo "Client was not properly built."
      exit 1
   fi
-markEndOfBlock "Build the StarterProject (Windows)"
+markEndOfBlock "Build the Starter project (Windows)"
 
 markEndOfBlock "$0"
