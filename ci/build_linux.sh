d4ac8766d72d0ccbbd054c7931495ea807ed8b56
INTERNAL\danielhall <danielhall@improbable.io>
2018-09-27 16:17:05 +0100

    New CI scripts

--- ci/build_linux.sh
+++ ci/build_linux.sh
@@ -10,19 +10,19 @@ markStartOfBlock "$0"
 
 source ci/prepare_build_environment.sh
 
-markStartOfBlock "Build the StarterProject (Linux)"
+markStartOfBlock "Build the Starter project (Linux)"
   # Build each target to ensure scripts are correct, skipping code generation on all but the first to save some time.
-  Game/Scripts/BuildWorker.bat "StarterProjectEditor" "Win64" "Development" "StarterProject.uproject"
+  Game/Plugins/UnrealGDK/SpatialGDK/Build/Scripts/BuildWorker.bat "StarterProjectEditor" "Win64" "Development" "StarterProject.uproject"
   if [[ ! -f "spatial/build/assembly/worker/UnrealEditor@Windows.zip" ]]; then
     echo "Editor was not properly built."
     exit 1
   fi
 
-  Game/Scripts/BuildWorker.bat "StarterProjectServer" "Linux" "Development" "StarterProject.uproject" --skip-codegen
+  Game/Plugins/UnrealGDK/SpatialGDK/Build/Scripts/BuildWorker.bat "StarterProjectServer" "Linux" "Development" "StarterProject.uproject" --skip-codegen
   if [[ ! -f "spatial/build/assembly/worker/UnrealWorker@Linux.zip" ]]; then
     echo "Linux Server was not properly built."
     exit 1
   fi
-markEndOfBlock "Build the StarterProject (Linux)"
+markEndOfBlock "Build the Starter project (Linux)"
 
 markEndOfBlock "$0"
