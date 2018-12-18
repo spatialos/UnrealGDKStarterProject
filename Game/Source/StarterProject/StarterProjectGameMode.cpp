431a2ae0d49091593f9c3434171dd3d1f152c257
Dave Dolben <davedolben@improbable.io>
2018-12-17 17:38:58 -0800

    [WIP] Replication throttling spike

--- Game/Source/StarterProject/StarterProjectGameMode.cpp
+++ Game/Source/StarterProject/StarterProjectGameMode.cpp
@@ -3,8 +3,15 @@
 #include "StarterProjectGameMode.h"
 #include "UObject/ConstructorHelpers.h"
 
+#include "EngineClasses/SpatialNetConnection.h"
+#include "EngineClasses/SpatialNetDriver.h"
+#include "Engine/World.h"
+#include "Interop/Connection/SpatialWorkerConnection.h"
+
 AStarterProjectGameMode::AStarterProjectGameMode()
 {
+	PrimaryActorTick.bCanEverTick = true;
+
 	//set default pawn class to our Blueprinted character
 	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/StarterProject/Characters/PlayerCharacter_BP"));
 	if (PlayerPawnBPClass.Class != NULL)
@@ -14,4 +21,22 @@ AStarterProjectGameMode::AStarterProjectGameMode()
 	
 	// Seamless Travel is not currently supported in SpatialOS [UNR-897]
 	bUseSeamlessTravel = false;
+}
+
+void AStarterProjectGameMode::Tick(float DeltaSeconds)
+{
+	if (GetNetMode() == NM_DedicatedServer && !bHasUpdatedMaxActorsToReplicate)
+	{
+		if (USpatialNetDriver* SND = Cast<USpatialNetDriver>(GetWorld()->GetNetDriver()))
+		{
+			if (SND->Connection && SND->Connection->IsConnected())
+			{
+				if (USpatialNetConnection* SNC = SND->GetSpatialOSNetConnection())
+				{
+					SNC->SetMaxActorsToReplicatePerFrame(MaxActorsToReplicate);
+					bHasUpdatedMaxActorsToReplicate = true;
+				}
+			}
+		}
+	}
 }
