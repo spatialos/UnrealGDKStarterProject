07f44a0ee4521051713b660df9ddc8da1b05685e
Daniel Hall <danielhall@improbable.io>
2019-03-15 20:27:06 +0000

    Updated changes

--- Game/Source/StarterProject/StarterProject.Build.cs
+++ Game/Source/StarterProject/StarterProject.Build.cs
@@ -18,7 +18,8 @@ public class StarterProject : ModuleRules
 				"Sockets",
 				"OnlineSubsystemUtils",
 				"PhysXVehicles",
-				"SpatialGDK"
+				"SpatialGDK",
+                "AIModule"
 			});
 	}
 }
